5dca5bed57d6de9b437592a3c6f57401861e38d5
Brian Strobach <brian@appsaurus.io>
2019-03-21 11:18:32 -0500

    Fixing broken border API

--- Sources/Shared/UIView/UIView+Borders.swift
+++ Sources/Shared/UIView/UIView+Borders.swift
@@ -15,10 +15,6 @@ extension UIView {
                           color: UIColor = UIColor.white,
                           thickness: CGFloat = 1,
                           padding: CGFloat = 0.0) -> [UIView] {
-
-        var thickness = thickness
-        thickness = (thickness / UIScreen.main.scale)
-
         var borders = [UIView]()
 
         func border() -> UIView {
@@ -30,40 +26,45 @@ extension UIView {
         if edges.contains(.top) || edges.contains(.all) {
             let top = border()
             addSubview(top)
-            top.topAnchor.constraint(equalTo: topAnchor, constant: padding)
-            top.leftAnchor.constraint(equalTo: leftAnchor, constant: padding)
-            top.rightAnchor.constraint(equalTo: rightAnchor, constant: padding)
+            top.topAnchor.constraint(equalTo: topAnchor, constant: padding).isActive = true
+            top.leftAnchor.constraint(equalTo: leftAnchor, constant: padding).isActive = true
+            top.rightAnchor.constraint(equalTo: rightAnchor, constant: -padding).isActive = true
+            top.heightAnchor.constraint(equalToConstant: thickness).isActive = true
             borders.append(top)
         }
 
         if edges.contains(.left) || edges.contains(.all) {
             let left = border()
             addSubview(left)
-            left.topAnchor.constraint(equalTo: topAnchor, constant: padding)
-            left.leftAnchor.constraint(equalTo: leftAnchor, constant: padding)
-            left.bottomAnchor.constraint(equalTo: bottomAnchor, constant: padding)
+            left.topAnchor.constraint(equalTo: topAnchor, constant: padding).isActive = true
+            left.leftAnchor.constraint(equalTo: leftAnchor, constant: padding).isActive = true
+            left.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -padding).isActive = true
+            left.widthAnchor.constraint(equalToConstant: thickness).isActive = true
             borders.append(left)
         }
 
         if edges.contains(.right) || edges.contains(.all) {
             let right = border()
             addSubview(right)
-            right.topAnchor.constraint(equalTo: topAnchor, constant: padding)
-            right.bottomAnchor.constraint(equalTo: bottomAnchor, constant: padding)
-            right.rightAnchor.constraint(equalTo: rightAnchor, constant: padding)
+            right.topAnchor.constraint(equalTo: topAnchor, constant: padding).isActive = true
+            right.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -padding).isActive = true
+            right.rightAnchor.constraint(equalTo: rightAnchor, constant: -padding).isActive = true
+            right.widthAnchor.constraint(equalToConstant: thickness).isActive = true
             borders.append(right)
         }
 
         if edges.contains(.bottom) || edges.contains(.all) {
             let bottom = border()
             addSubview(bottom)
-            bottom.bottomAnchor.constraint(equalTo: bottomAnchor, constant: padding)
-            bottom.leftAnchor.constraint(equalTo: leftAnchor, constant: padding)
-            bottom.rightAnchor.constraint(equalTo: rightAnchor, constant: padding)
+            bottom.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -padding).isActive = true
+            bottom.leftAnchor.constraint(equalTo: leftAnchor, constant: padding).isActive = true
+            bottom.rightAnchor.constraint(equalTo: rightAnchor, constant: -padding).isActive = true
+            bottom.heightAnchor.constraint(equalToConstant: thickness).isActive = true
             borders.append(bottom)
         }
-        borders.forEach { (b) in
-            b.heightAnchor.constraint(equalToConstant: thickness)
+
+        borders.forEach{ b in
+            b.translatesAutoresizingMaskIntoConstraints = false
         }
         return borders
     }
